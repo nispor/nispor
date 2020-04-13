7102e4edb79861305d6a973f93ad75a2f37df1c8
Gris Ge <cnfourt@gmail.com>
2020-04-13 09:21:54 +0800

    Support querying name and MTU via netlink
    Signed-off-by: Gris Ge <cnfourt@gmail.com>

--- src/lib/net_state.rs
+++ src/lib/net_state.rs
@@ -1,16 +1,16 @@
 use crate::error::ZatelError;
 use crate::iface_state::get_ifaces;
-use crate::iface_state::IfaceState;
+use crate::iface_state::Iface;
 use serde_derive::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 pub struct NetState {
-    pub iface_states: HashMap<String, IfaceState>,
+    pub ifaces: HashMap<String, Iface>,
 }
 
 pub fn get_state() -> Result<NetState, ZatelError> {
     Ok(NetState {
-        iface_states: get_ifaces(),
+        ifaces: get_ifaces(),
     })
 }
