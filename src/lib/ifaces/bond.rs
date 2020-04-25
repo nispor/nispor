b354696c681232965f1fe260375fd46a47ac2847
Gris Ge <cnfourt@gmail.com>
2020-04-25 11:31:56 +0800

    Bond: Add BondMode enum
    Signed-off-by: Gris Ge <cnfourt@gmail.com>

--- src/lib/ifaces/bond.rs
+++ src/lib/ifaces/bond.rs
@@ -2,9 +2,28 @@ use netlink_packet_route::rtnl::link::nlas;
 use serde_derive::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+pub enum BondMode {
+    RoundRobin,
+    ActiveBackup,
+    BalanceXOR,
+    Broadcast,
+    IEEE8023AD,
+    AdaptiveTransmitLoadBalancing,
+    AdaptiveLoadBalancing,
+    Unknown,
+}
+
+impl Default for BondMode {
+    fn default() -> Self {
+        return BondMode::Unknown;
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone, Default)]
 pub struct BondConf {
     pub slaves: Vec<String>,
+    pub mode: BondMode,
     pub options: HashMap<String, String>,
 }
 
