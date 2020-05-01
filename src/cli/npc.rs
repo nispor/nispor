a44354ad6c6daa140320d548d3fca1e6324868dc
Gris Ge <cnfourt@gmail.com>
2020-05-02 03:10:32 +0800

    cli: Don't panic when interface not found
    Signed-off-by: Gris Ge <cnfourt@gmail.com>

--- src/cli/npc.rs
+++ src/cli/npc.rs
@@ -1,13 +1,17 @@
+use nispor::get_state;
 use serde_json;
 use std::env::args;
-use nispor::get_state;
 
 fn main() {
     let argv: Vec<String> = args().collect();
     if let Ok(state) = get_state() {
         if argv.len() > 1 {
-            let iface = &state.ifaces[&argv[1]];
-            println!("{}", serde_json::to_string_pretty(iface).unwrap());
+            match &state.ifaces.get(&argv[1]) {
+                Some(iface) => {
+                    println!("{}", serde_json::to_string_pretty(iface).unwrap())
+                }
+                None => eprintln!("Interface '{}' not found", argv[1]),
+            }
         } else {
             println!("{}", serde_json::to_string_pretty(&state).unwrap());
         }
