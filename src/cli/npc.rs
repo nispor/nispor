b3608fb1b85a2bb440f4f6eed485064b94a30307
Gris Ge <fge@redhat.com>
2020-07-26 20:15:32 +0800

    cli: Print error message when failed
    Signed-off-by: Gris Ge <fge@redhat.com>

--- src/cli/npc.rs
+++ src/cli/npc.rs
@@ -4,16 +4,21 @@ use std::env::args;
 
 fn main() {
     let argv: Vec<String> = args().collect();
-    if let Ok(state) = get_state() {
-        if argv.len() > 1 {
-            match &state.ifaces.get(&argv[1]) {
-                Some(iface) => {
-                    println!("{}", serde_json::to_string_pretty(iface).unwrap())
+    match get_state() {
+        Ok(state) => {
+            if argv.len() > 1 {
+                match &state.ifaces.get(&argv[1]) {
+                    Some(iface) => {
+                        println!("{}", serde_json::to_string_pretty(iface).unwrap())
+                    }
+                    None => eprintln!("Interface '{}' not found", argv[1]),
                 }
-                None => eprintln!("Interface '{}' not found", argv[1]),
+            } else {
+                println!("{}", serde_json::to_string_pretty(&state).unwrap());
             }
-        } else {
-            println!("{}", serde_json::to_string_pretty(&state).unwrap());
+        },
+        Err(e) => {
+            eprintln!("{}", e);
         }
     }
 }
