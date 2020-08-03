42b5830cf7a820a832230a848dae5d296ec18775
Gris Ge <fge@redhat.com>
2020-08-04 00:30:05 +0800

    VxLAN: Add missing source files
    Signed-off-by: Gris Ge <fge@redhat.com>

---
+++ src/lib/ifaces/vxlan.rs
@@ -0,0 +1,44 @@
+use crate::netlink::parse_vxlan_info;
+use netlink_packet_route::rtnl::link::nlas::InfoData;
+use serde_derive::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone, Default)]
+pub struct VxlanInfo {
+    pub remote: String,
+    pub vxlan_id: u32,
+    pub base_iface: String,
+    pub local: String,
+    pub ttl: u8,
+    pub tos: u8,
+    pub learning: bool,
+    pub ageing: u32,
+    pub max_address: u32,
+    pub src_port_min: u16,
+    pub src_port_max: u16,
+    pub proxy: bool,
+    pub rsc: bool,
+    pub l2miss: bool,
+    pub l3miss: bool,
+    pub dst_port_min: u16,
+    pub dst_port_max: u16,
+    pub udp_check_sum: bool,
+    pub udp6_zero_check_sum_tx: bool,
+    pub udp6_zero_check_sum_rx: bool,
+    pub remote_check_sum_tx: bool,
+    pub remote_check_sum_rx: bool,
+    pub gbp: bool,
+    pub remote_check_sum_no_partial: bool,
+    pub collect_metadata: bool,
+    pub label: u32,
+    pub gpe: bool,
+    pub ttl_inherit: bool,
+    pub df: u8,
+}
+
+pub(crate) fn get_vxlan_info(data: &InfoData) -> Option<VxlanInfo> {
+    if let InfoData::Vxlan(raw) = data {
+        Some(parse_vxlan_info(&raw))
+    } else {
+        None
+    }
+}
