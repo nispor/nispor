47e603d65308c0644313be9042b2ba2f69d9034a
Gris Ge <fge@redhat.com>
2020-08-16 12:28:58 +0800

    Python: Use setuptools
    Signed-off-by: Gris Ge <fge@redhat.com>

---
+++ src/python/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="nispor",
+    version="0.2.1",
+    author="Gris Ge",
+    author_email="fge@redhat.com",
+    description="Python binding of Nispor",
+    long_description="Python binding of Nipor for kernel querying network state",
+    url="https://github.com/nispor/nispor/",
+    packages=setuptools.find_packages(),
+    license="ASL2.0+",
+    python_requires='>=3.6',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+    ],
+)
+
