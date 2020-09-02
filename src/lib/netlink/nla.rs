7db7c1773b47b4eafa9193e36370fad8a9615e2e
Quique Llorente <ellorent@redhat.com>
2020-09-02 13:42:20 +0200

    vxlan.rs: Use `NlasIterator` and `NlaBuffer` instead of `parse_nla_header`
    Signed-off-by: Quique Llorente <ellorent@redhat.com>

--- src/lib/netlink/nla.rs
+++ src/lib/netlink/nla.rs
@@ -1,36 +1,11 @@
-use std::mem::transmute;
 use std::net::Ipv4Addr;
 use std::net::Ipv6Addr;
-
-pub(crate) const NL_ATTR_HDR_LEN: usize = 4;
-pub(crate) const NLA_ALIGNTO: usize = 4;
 
 #[derive(Debug, Eq, PartialEq, Clone)]
 pub(crate) struct NetLinkAttrHeader {
     pub data_len: usize,
     pub nla_len: usize,
     pub nla_type: u16,
-}
-
-pub(crate) fn parse_nla_header(data: *const u8) -> NetLinkAttrHeader {
-    let mut data_len: usize =
-        unsafe { u16::from_ne_bytes([*data, *(data.wrapping_offset(1))]) }
-            .into();
-    let nla_type: u16 = unsafe {
-        transmute::<[u8; 2], u16>([
-            *(data.wrapping_offset(2)),
-            *(data.wrapping_offset(3)),
-        ])
-    };
-
-    // Align nla_len by NLA_ALIGNTO
-    let nla_len = ((data_len + NLA_ALIGNTO - 1) / NLA_ALIGNTO) * NLA_ALIGNTO;
-    data_len = data_len - NL_ATTR_HDR_LEN;
-    NetLinkAttrHeader {
-        data_len,
-        nla_len,
-        nla_type,
-    }
 }
 
 pub(crate) fn parse_as_u8(data: &[u8]) -> u8 {
--- src/lib/netlink/vxlan.rs
+++ src/lib/netlink/vxlan.rs
@@ -5,10 +5,7 @@ use crate::netlink::nla::parse_as_ipv4;
 use crate::netlink::nla::parse_as_ipv6;
 use crate::netlink::nla::parse_as_u32;
 use crate::netlink::nla::parse_as_u8;
-use crate::netlink::nla::parse_nla_header;
-use crate::netlink::nla::NL_ATTR_HDR_LEN;
-use std::convert::TryInto;
-use std::slice;
+use netlink_packet_route::rtnl::nlas::NlasIterator;
 
 const IFLA_VXLAN_ID: u16 = 1;
 const IFLA_VXLAN_GROUP: u16 = 2;
@@ -40,111 +37,109 @@ const IFLA_VXLAN_GPE: u16 = 27;
 const IFLA_VXLAN_TTL_INHERIT: u16 = 28;
 const IFLA_VXLAN_DF: u16 = 29;
 
-pub(crate) fn parse_vxlan_info(data: &[u8]) -> VxlanInfo {
+pub(crate) fn parse_vxlan_info(raw: &[u8]) -> VxlanInfo {
+    let nlas = NlasIterator::new(raw);
     let mut info = VxlanInfo::default();
-    let mut i: usize = 0;
-    while i < data.len() {
-        let hdr_ptr = data.as_ptr().wrapping_offset(i.try_into().unwrap());
-        let hdr = parse_nla_header(hdr_ptr);
-        let data_ptr = data
-            .as_ptr()
-            .wrapping_offset((i + NL_ATTR_HDR_LEN).try_into().unwrap());
-        let data = unsafe {
-            slice::from_raw_parts(data_ptr, hdr.nla_len - NL_ATTR_HDR_LEN)
-        };
-        match hdr.nla_type {
-            IFLA_VXLAN_ID => {
-                info.vxlan_id = parse_as_u32(data);
+    for nla in nlas {
+        match nla {
+            Ok(nla) => {
+                match nla.kind() {
+                    IFLA_VXLAN_ID => {
+                        info.vxlan_id = parse_as_u32(nla.value());
+                    }
+                    IFLA_VXLAN_GROUP => {
+                        info.remote = parse_as_ipv4(nla.value());
+                    }
+                    IFLA_VXLAN_LINK => {
+                        info.base_iface = format!("{}", parse_as_u32(nla.value()));
+                    }
+                    IFLA_VXLAN_LOCAL => {
+                        info.local = parse_as_ipv4(nla.value());
+                    }
+                    IFLA_VXLAN_TTL => {
+                        info.ttl = parse_as_u8(nla.value());
+                    }
+                    IFLA_VXLAN_TOS => {
+                        info.tos = parse_as_u8(nla.value());
+                    }
+                    IFLA_VXLAN_LEARNING => {
+                        info.learning = parse_as_u8(nla.value()) > 0;
+                    }
+                    IFLA_VXLAN_AGEING => {
+                        info.ageing = parse_as_u32(nla.value());
+                    }
+                    IFLA_VXLAN_LIMIT => {
+                        info.max_address = parse_as_u32(nla.value());
+                    }
+                    IFLA_VXLAN_PORT_RANGE => {
+                        info.src_port_min = parse_as_be16(nla.value());
+                        info.src_port_max = parse_as_be16(&[nla.value()[2], nla.value()[3]]);
+                    }
+                    IFLA_VXLAN_PROXY => {
+                        info.proxy = parse_as_u8(nla.value()) > 0;
+                    }
+                    IFLA_VXLAN_RSC => {
+                        info.rsc = parse_as_u8(nla.value()) > 0;
+                    }
+                    IFLA_VXLAN_L2MISS => {
+                        info.l2miss = parse_as_u8(nla.value()) > 0;
+                    }
+                    IFLA_VXLAN_L3MISS => {
+                        info.l3miss = parse_as_u8(nla.value()) > 0;
+                    }
+                    IFLA_VXLAN_PORT => {
+                        info.dst_port_min = parse_as_be16(nla.value());
+                        info.dst_port_max = parse_as_be16(&[nla.value()[2], nla.value()[3]]);
+                    }
+                    IFLA_VXLAN_GROUP6 => {
+                        info.remote = parse_as_ipv6(nla.value());
+                    }
+                    IFLA_VXLAN_LOCAL6 => {
+                        info.local = parse_as_ipv6(nla.value());
+                    }
+                    IFLA_VXLAN_UDP_CSUM => {
+                        info.udp_check_sum = parse_as_u8(nla.value()) > 0;
+                    }
+                    IFLA_VXLAN_UDP_ZERO_CSUM6_TX => {
+                        info.udp6_zero_check_sum_tx = parse_as_u8(nla.value()) > 0;
+                    }
+                    IFLA_VXLAN_UDP_ZERO_CSUM6_RX => {
+                        info.udp6_zero_check_sum_rx = parse_as_u8(nla.value()) > 0;
+                    }
+                    IFLA_VXLAN_REMCSUM_TX => {
+                        info.remote_check_sum_tx = parse_as_u8(nla.value()) > 0;
+                    }
+                    IFLA_VXLAN_REMCSUM_RX => {
+                        info.remote_check_sum_rx = parse_as_u8(nla.value()) > 0;
+                    }
+                    IFLA_VXLAN_GBP => {
+                        info.gbp = true;
+                    }
+                    IFLA_VXLAN_REMCSUM_NOPARTIAL => {
+                        info.remote_check_sum_no_partial = true;
+                    }
+                    IFLA_VXLAN_COLLECT_METADATA => {
+                        info.collect_metadata = parse_as_u8(nla.value()) > 0;
+                    }
+                    IFLA_VXLAN_LABEL => {
+                        info.label = parse_as_be32(nla.value());
+                    }
+                    IFLA_VXLAN_GPE => {
+                        info.gpe = true;
+                    }
+                    IFLA_VXLAN_TTL_INHERIT => {
+                        info.ttl_inherit = parse_as_u8(nla.value()) > 0;
+                    }
+                    IFLA_VXLAN_DF => {
+                        info.df = parse_as_u8(nla.value());
+                    }
+                    _ => eprintln!("Unhandled VxLAN IFLA_INFO_DATA: {}, {:?}", nla.kind(), nla.value()),
+                }
             }
-            IFLA_VXLAN_GROUP => {
-                info.remote = parse_as_ipv4(data);
+            Err(e) => {
+                    eprintln!("{}", e)
             }
-            IFLA_VXLAN_LINK => {
-                info.base_iface = format!("{}", parse_as_u32(data));
-            }
-            IFLA_VXLAN_LOCAL => {
-                info.local = parse_as_ipv4(data);
-            }
-            IFLA_VXLAN_TTL => {
-                info.ttl = parse_as_u8(data);
-            }
-            IFLA_VXLAN_TOS => {
-                info.tos = parse_as_u8(data);
-            }
-            IFLA_VXLAN_LEARNING => {
-                info.learning = parse_as_u8(data) > 0;
-            }
-            IFLA_VXLAN_AGEING => {
-                info.ageing = parse_as_u32(data);
-            }
-            IFLA_VXLAN_LIMIT => {
-                info.max_address = parse_as_u32(data);
-            }
-            IFLA_VXLAN_PORT_RANGE => {
-                info.src_port_min = parse_as_be16(data);
-                info.src_port_max = parse_as_be16(&[data[2], data[3]]);
-            }
-            IFLA_VXLAN_PROXY => {
-                info.proxy = parse_as_u8(data) > 0;
-            }
-            IFLA_VXLAN_RSC => {
-                info.rsc = parse_as_u8(data) > 0;
-            }
-            IFLA_VXLAN_L2MISS => {
-                info.l2miss = parse_as_u8(data) > 0;
-            }
-            IFLA_VXLAN_L3MISS => {
-                info.l3miss = parse_as_u8(data) > 0;
-            }
-            IFLA_VXLAN_PORT => {
-                info.dst_port_min = parse_as_be16(data);
-                info.dst_port_max = parse_as_be16(&[data[2], data[3]]);
-            }
-            IFLA_VXLAN_GROUP6 => {
-                info.remote = parse_as_ipv6(data);
-            }
-            IFLA_VXLAN_LOCAL6 => {
-                info.local = parse_as_ipv6(data);
-            }
-            IFLA_VXLAN_UDP_CSUM => {
-                info.udp_check_sum = parse_as_u8(data) > 0;
-            }
-            IFLA_VXLAN_UDP_ZERO_CSUM6_TX => {
-                info.udp6_zero_check_sum_tx = parse_as_u8(data) > 0;
-            }
-            IFLA_VXLAN_UDP_ZERO_CSUM6_RX => {
-                info.udp6_zero_check_sum_rx = parse_as_u8(data) > 0;
-            }
-            IFLA_VXLAN_REMCSUM_TX => {
-                info.remote_check_sum_tx = parse_as_u8(data) > 0;
-            }
-            IFLA_VXLAN_REMCSUM_RX => {
-                info.remote_check_sum_rx = parse_as_u8(data) > 0;
-            }
-            IFLA_VXLAN_GBP => {
-                info.gbp = true;
-            }
-            IFLA_VXLAN_REMCSUM_NOPARTIAL => {
-                info.remote_check_sum_no_partial = true;
-            }
-            IFLA_VXLAN_COLLECT_METADATA => {
-                info.collect_metadata = parse_as_u8(data) > 0;
-            }
-            IFLA_VXLAN_LABEL => {
-                info.label = parse_as_be32(data);
-            }
-            IFLA_VXLAN_GPE => {
-                info.gpe = true;
-            }
-            IFLA_VXLAN_TTL_INHERIT => {
-                info.ttl_inherit = parse_as_u8(data) > 0;
-            }
-            IFLA_VXLAN_DF => {
-                info.df = parse_as_u8(data);
-            }
-            _ => eprintln!("Unknwon nla_type {}, {:?}", hdr.nla_type, data),
         }
-        i = i + hdr.nla_len;
     }
     info
 }
