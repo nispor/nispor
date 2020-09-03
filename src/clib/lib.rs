nispor/nispor

commit 5898a6f82c7bb41a8c25f3110b5143f0bd832139

clib: Fix unsound free usage
CString should not be freed by ordinary libc free
but by using CString drop function. This will prevent
issues on systems that use custom allocators.

src/clib/lib.rs
@@ -37,15 +37,15 @@ pub extern "C" fn nispor_state_get(
 
 #[no_mangle]
 pub extern "C" fn nispor_state_free(state: *mut c_char) {
-    unsafe { libc::free(state as *mut libc::c_void) }
+    unsafe { CString::from_raw(state) };
 }
 
 #[no_mangle]
 pub extern "C" fn nispor_err_kind_free(err_kind: *mut c_char) {
-    unsafe { libc::free(err_kind as *mut libc::c_void) }
+    unsafe { CString::from_raw(err_kind) };
 }
 
 #[no_mangle]
 pub extern "C" fn nispor_err_msg_free(err_msg: *mut c_char) {
-    unsafe { libc::free(err_msg as *mut libc::c_void) }
+    unsafe { CString::from_raw(err_msg) };
 }

commit f023c9a6e8a970435239d964462e586af80b1fe7

clib: fix free usage
Commit 5898a6f introduced a segmentation fault because
CStrings would get dropped (freed) even if they were null pointers.

Signed-off-by: Antonio Cardace <acardace@redhat.com>

src/clib/lib.rs
@@ -37,15 +37,27 @@ pub extern "C" fn nispor_state_get(
 
 #[no_mangle]
 pub extern "C" fn nispor_state_free(state: *mut c_char) {
-    unsafe { CString::from_raw(state) };
+    unsafe {
+        if state != std::ptr::null_mut() {
+            CString::from_raw(state);
+        }
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn nispor_err_kind_free(err_kind: *mut c_char) {
-    unsafe { CString::from_raw(err_kind) };
+    unsafe {
+        if err_kind != std::ptr::null_mut() {
+            CString::from_raw(err_kind);
+        }
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn nispor_err_msg_free(err_msg: *mut c_char) {
-    unsafe { CString::from_raw(err_msg) };
+    unsafe {
+        if err_msg != std::ptr::null_mut() {
+            CString::from_raw(err_msg);
+        }
+    }
 }