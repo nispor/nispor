7245b59b61f2e1b6f71b3e25feb3f26828217da3
Gris Ge <fge@redhat.com>
2022-03-30 22:00:23 +0800

    Revert "bridge: Use patched netlink-packet-route as workaround"
    This reverts commit 59b8d1124756dabaeccac870e8664619c0349ebe.

--- src/netlink-packet-route/fuzz/fuzz_targets/netlink.rs
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-#![no_main]
-use libfuzzer_sys::fuzz_target;
-use netlink_packet_route::{NetlinkMessage, RtnlMessage};
-
-fuzz_target!(|data: &[u8]| {
-    let _ = NetlinkMessage::<RtnlMessage>::deserialize(data);
-});
