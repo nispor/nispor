nispor/nispor

commit 25f186819f0ef590764cd9c3937a8319d6dd171a

Remove all usages of "expect"
Replace them with proper error handling

Signed-off-by: Íñigo Huguet <ihuguet@redhat.com>

src/lib/mac.rs
@@ -18,14 +18,12 @@ pub(crate) fn parse_as_mac(
     mac_len: usize,
     data: &[u8],
 ) -> Result<String, NisporError> {
+    if data.len() != mac_len {
+        return Err(NisporError::bug("wrong size at mac parsing".into()));
+    }
     let mut rt = String::new();
     for i in 0..mac_len {
-        rt.push_str(&format!(
-            "{:02x}",
-            *data.get(i).ok_or_else(|| NisporError::bug(
-                "wrong index at mac parsing".into()
-            ))?
-        ));
+        rt.push_str(&format!("{:02x}", data[i]));
         if i != mac_len - 1 {
             rt.push(':');
         }

src/lib/ifaces/vxlan.rs
@@ -64,15 +64,15 @@ pub(crate) fn get_vxlan_info(
             if let InfoVxlan::Id(d) = *info {
                 vxlan_info.vxlan_id = d;
             } else if let InfoVxlan::Group(d) = info {
-                vxlan_info.remote = parse_as_ipv4(d).to_string();
+                vxlan_info.remote = parse_as_ipv4(d)?.to_string();
             } else if let InfoVxlan::Group6(d) = info {
-                vxlan_info.remote = parse_as_ipv6(d).to_string();
+                vxlan_info.remote = parse_as_ipv6(d)?.to_string();
             } else if let InfoVxlan::Link(d) = *info {
                 vxlan_info.base_iface = format!("{}", d);
             } else if let InfoVxlan::Local(d) = info {
-                vxlan_info.local = parse_as_ipv4(d).to_string();
+                vxlan_info.local = parse_as_ipv4(d)?.to_string();
             } else if let InfoVxlan::Local6(d) = info {
-                vxlan_info.local = parse_as_ipv6(d).to_string();
+                vxlan_info.local = parse_as_ipv6(d)?.to_string();
             } else if let InfoVxlan::Tos(d) = *info {
                 vxlan_info.tos = d;
             } else if let InfoVxlan::Ttl(d) = *info {

src/lib/netlink/bond.rs
@@ -35,7 +35,10 @@ fn parse_as_nested_ipv4_addr(raw: &[u8]) -> Vec<Ipv4Addr> {
     let nlas = NlasIterator::new(raw);
     for nla in nlas {
         match nla {
-            Ok(nla) => addresses.push(parse_as_ipv4(nla.value())),
+            Ok(nla) => match parse_as_ipv4(nla.value()) {
+                Ok(addr) => addresses.push(addr),
+                Err(e) => log::warn!("{}", e),
+            },
             Err(e) => {
                 log::warn!("{}", e);
             }

src/lib/netlink/bridge.rs
@@ -136,7 +136,9 @@ fn parse_bridge_id(
             "wrong index at bridge_id parsing".into()
         ))?,
         parse_as_mac(ETH_ALEN, mac)
-            .expect("error when parsing mac address in bridge_id")
+            .map_err(|_| NisporError::invalid_argument(
+                "invalid mac address in bridge_id".into()
+            ))?
             .to_lowercase()
             .replace(':', "")
     ))

src/lib/netlink/ip.rs
@@ -95,9 +95,9 @@ fn parse_ipv4_nlas(
     let mut peer = String::new();
     for nla in &nl_msg.nlas {
         if let Nla::Local(addr_vec) = nla {
-            addr.address = parse_as_ipv4(addr_vec.as_slice()).to_string();
+            addr.address = parse_as_ipv4(addr_vec.as_slice())?.to_string();
         } else if let Nla::Address(addr_vec) = nla {
-            peer = parse_as_ipv4(addr_vec.as_slice()).to_string();
+            peer = parse_as_ipv4(addr_vec.as_slice())?.to_string();
         } else if let Nla::CacheInfo(cache_info_vec) = nla {
             let cache_info = CacheInfo::parse(&CacheInfoBuffer::new(
                 cache_info_vec.as_slice(),
@@ -125,7 +125,7 @@ fn parse_ipv6_nlas(
 
     for nla in &nl_msg.nlas {
         if let Nla::Address(addr_vec) = nla {
-            addr.address = parse_as_ipv6(addr_vec.as_slice()).to_string();
+            addr.address = parse_as_ipv6(addr_vec.as_slice())?.to_string();
         } else if let Nla::CacheInfo(cache_info_vec) = nla {
             let cache_info = CacheInfo::parse(&CacheInfoBuffer::new(
                 cache_info_vec.as_slice(),

src/lib/netlink/nla.rs
@@ -101,16 +101,20 @@ pub(crate) fn parse_as_u64(data: &[u8]) -> Result<u64, NisporError> {
     ]))
 }
 
-pub(crate) fn parse_as_ipv4(data: &[u8]) -> Ipv4Addr {
-    let addr_bytes: [u8; 4] = data
-        .try_into()
-        .expect("Got invalid IPv4 address u8, the length is not 4 ");
-    Ipv4Addr::from(addr_bytes)
+pub(crate) fn parse_as_ipv4(data: &[u8]) -> Result<Ipv4Addr, NisporError> {
+    let addr_bytes: [u8; 4] = data.try_into().map_err(|_| {
+        NisporError::invalid_argument(
+            "Got invalid IPv4 address u8, the length is not 4".into(),
+        )
+    })?;
+    Ok(Ipv4Addr::from(addr_bytes))
 }
 
-pub(crate) fn parse_as_ipv6(data: &[u8]) -> Ipv6Addr {
-    let addr_bytes: [u8; 16] = data
-        .try_into()
-        .expect("Got invalid IPv6 address u8, the length is not 16 ");
-    Ipv6Addr::from(addr_bytes)
+pub(crate) fn parse_as_ipv6(data: &[u8]) -> Result<Ipv6Addr, NisporError> {
+    let addr_bytes: [u8; 16] = data.try_into().map_err(|_| {
+        NisporError::invalid_argument(
+            "Got invalid IPv6 address u8, the length is not 16".into(),
+        )
+    })?;
+    Ok(Ipv6Addr::from(addr_bytes))
 }

src/lib/route.rs
@@ -449,7 +449,7 @@ fn get_route(
             Nla::Destination(ref d) => {
                 rt.dst = Some(format!(
                     "{}/{}",
-                    _addr_to_string(d, family),
+                    _addr_to_string(d, family)?,
                     dst_prefix_len
                 ));
             }
@@ -463,7 +463,7 @@ fn get_route(
                 }
             }
             Nla::PrefSource(ref d) => {
-                rt.prefered_src = Some(_addr_to_string(d, family));
+                rt.prefered_src = Some(_addr_to_string(d, family)?);
             }
             Nla::Table(d) => {
                 rt.table = *d;
@@ -474,15 +474,15 @@ fn get_route(
             Nla::Source(ref d) => {
                 rt.src = Some(format!(
                     "{}/{}",
-                    _addr_to_string(d, family),
+                    _addr_to_string(d, family)?,
                     src_prefix_len
                 ));
             }
             Nla::Gateway(ref d) => {
-                rt.gateway = Some(_addr_to_string(d, family));
+                rt.gateway = Some(_addr_to_string(d, family)?);
             }
             Nla::Via(ref d) => {
-                rt.via = Some(_addr_to_string(d, family));
+                rt.via = Some(_addr_to_string(d, family)?);
             }
             Nla::Metrics(ref d) => {
                 let nlas = NlasIterator::new(d);
@@ -600,7 +600,7 @@ fn get_route(
                         &d[i + SIZE_OF_RTNEXTHOP..i + nex_hop_len as usize],
                     );
                     let via = match nla.kind() {
-                        RTA_GATEWAY => _addr_to_string(nla.value(), family),
+                        RTA_GATEWAY => _addr_to_string(nla.value(), family)?,
                         RTA_VIA => {
                             // Kernel will use RTA_VIA when gateway family does
                             // not match nexthop family
@@ -676,12 +676,15 @@ fn get_route(
     Ok(rt)
 }
 
-fn _addr_to_string(data: &[u8], family: &AddressFamily) -> String {
-    match family {
-        AddressFamily::IPv4 => parse_as_ipv4(data).to_string(),
-        AddressFamily::IPv6 => parse_as_ipv6(data).to_string(),
+fn _addr_to_string(
+    data: &[u8],
+    family: &AddressFamily,
+) -> Result<String, NisporError> {
+    Ok(match family {
+        AddressFamily::IPv4 => parse_as_ipv4(data)?.to_string(),
+        AddressFamily::IPv6 => parse_as_ipv6(data)?.to_string(),
         _ => format!("{:?}", data),
-    }
+    })
 }
 
 #[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]

src/lib/route_rule.rs
@@ -224,8 +224,8 @@ fn _addr_to_string(
     family: &AddressFamily,
 ) -> Result<String, NisporError> {
     match family {
-        AddressFamily::IPv4 => Ok(parse_as_ipv4(data).to_string()),
-        AddressFamily::IPv6 => Ok(parse_as_ipv6(data).to_string()),
+        AddressFamily::IPv4 => Ok(parse_as_ipv4(data)?.to_string()),
+        AddressFamily::IPv6 => Ok(parse_as_ipv6(data)?.to_string()),
         _ => Err(NisporError {
             kind: ErrorKind::NisporBug,
             msg: "The rule is not a valid IPv4 and IPv6 rule".to_string(),

commit 101f8fb020e293c87dbb6f7cf1fddc521499bf6f

lib: fix parse_as_mac() to accept longer macs to unbreak SR-IOV
The SR-IOV fields IFLA_VF_MAC and IFLA_VF_BROADCAST on netlink are
32 bytes long. It depends on the interface type whether this is should
be ethernet or infiniband. Currently we determine the correct length
based on other MAC addresses, but a follow up patch will change that to
make it dependent on the link type.

The bigger problem is that currently the parsing is strict. Note that
get_sriov_info() gets the expected mac_len and passes it on. Usually, it
will be 6 (ETH_ALEN). This causes the SR-IOV data to be rejected as invalid.

Fix that by relaxing the check.

Note that other parse functions like crate::netlink::parse_as_u32()
accept longer fields too. This is a common pattern in netlink to
ignore extra bytes. Do that also for crate::mac::parse_as_mac().
If the function would be called crate::netlink::parse_as_mac(),
it would be clearer that this behavior is correct. For
crate::mac::parse_as_mac() it's not clear that extra bytes should be
ignored, but it probably makes sense.

https://bugzilla.redhat.com/show_bug.cgi?id=2090678

Fixes: 25f186819f0e ('Remove all usages of "expect"')

Signed-off-by: Thomas Haller <thaller@redhat.com>

src/lib/mac.rs
@@ -20,12 +20,12 @@ pub(crate) fn parse_as_mac(
     mac_len: usize,
     data: &[u8],
 ) -> Result<String, NisporError> {
-    if data.len() != mac_len {
+    if data.len() < mac_len {
         return Err(NisporError::bug("wrong size at mac parsing".into()));
     }
     let mut rt = String::new();
-    for (i, &val) in data.iter().enumerate() {
-        write!(rt, "{:02x}", val).ok();
+    for (i, m) in data.iter().enumerate().take(mac_len) {
+        write!(rt, "{:02x}", m).ok();
         if i != mac_len - 1 {
             rt.push(':');
         }