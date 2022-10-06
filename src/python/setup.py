5601f745bed84cc55a385ec55fa347832ecc1a72
Gris Ge <fge@redhat.com>
2022-10-06 09:40:19 +0800

    New release 1.2.8
    == Break changes
    
     * N/A
    
    == New features
    
     * Add support of filtering via `NetStateFilter`. (f128da4, c130bb6)
     * SR-IOV: Include SRIOV VF information per VF interface. (fd3c489)
     * New bond options: (77ede43)
       * `BondInfo.lacp_active`
       * `BondInfo.missed_max`
       * `BondInfo.ns_ip6_target`
    
    == Bug fixes
    
     * Expose `MptcpAddressFlag`. (dd2a427)
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- src/python/setup.py
+++ src/python/setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="nispor",
-    version="1.2.7",
+    version="1.2.8",
     author="Gris Ge",
     author_email="fge@redhat.com",
     description="Python binding of Nispor",
