dbba6aba3fc48b2b4b2afa8a67f4d09a279e3cfd
Gris Ge <fge@redhat.com>
2023-09-18 17:56:00 +0800

    New release 1.2.14
    == Break changes
     * N/A
    
    == New features
     * Support MACsec interface. (184cfcf, 528753e)
     * Support changing bond mode. (30a27af)
    
    == Bug fixes
     * N/A
    
    Signed-off-by: Gris Ge <fge@redhat.com>

--- src/python/setup.py
+++ src/python/setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="nispor",
-    version="1.2.13",
+    version="1.2.14",
     author="Gris Ge",
     author_email="fge@redhat.com",
     description="Python binding of Nispor",
