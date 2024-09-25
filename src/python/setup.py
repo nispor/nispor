efaa1c2b0d9852f53376da0492b3517d45e3cdf4
Gris Ge <fge@redhat.com>
2024-09-25 11:21:54 +0800

    Bump version to 1.2.21
    Signed-off-by: Gris Ge <fge@redhat.com>

--- src/python/setup.py
+++ src/python/setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="nispor",
-    version="1.2.20",
+    version="1.2.21",
     author="Gris Ge",
     author_email="fge@redhat.com",
     description="Python binding of Nispor",
