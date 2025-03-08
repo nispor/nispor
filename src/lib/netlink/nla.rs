56d058b70cbea5932d1519404c0eb09ee123b02d
Fernando Fernandez Mancera <ffmancera@riseup.net>
2025-03-08 12:02:44 +0100

    vxlan: stop using parse_as_ipv4/6
    As netlink-packet-route changed the vxlan group/6 and local/6 types from
    raw array of u8 to Ipv4Addr and Ipv6Addr we don't need to use the helper
    functions anymore.
    
    Signed-off-by: Fernando Fernandez Mancera <ffmancera@riseup.net>

--- src/lib/netlink/nla.rs
+++ src/lib/netlink/nla.rs
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use crate::NisporError;
-use std::net::Ipv4Addr;
-use std::net::Ipv6Addr;
 
 pub(crate) fn parse_as_u8(data: &[u8]) -> Result<u8, NisporError> {
     Ok(*data.first().ok_or_else(|| {
@@ -68,22 +66,4 @@ pub(crate) fn parse_as_u64(data: &[u8]) -> Result<u64, NisporError> {
             .get(7)
             .ok_or_else(|| NisporError::bug(err_msg.into()))?,
     ]))
-}
-
-pub(crate) fn parse_as_ipv4(data: &[u8]) -> Result<Ipv4Addr, NisporError> {
-    let addr_bytes: [u8; 4] = data.try_into().map_err(|_| {
-        NisporError::invalid_argument(
-            "Got invalid IPv4 address u8, the length is not 4".into(),
-        )
-    })?;
-    Ok(Ipv4Addr::from(addr_bytes))
-}
-
-pub(crate) fn parse_as_ipv6(data: &[u8]) -> Result<Ipv6Addr, NisporError> {
-    let addr_bytes: [u8; 16] = data.try_into().map_err(|_| {
-        NisporError::invalid_argument(
-            "Got invalid IPv6 address u8, the length is not 16".into(),
-        )
-    })?;
-    Ok(Ipv6Addr::from(addr_bytes))
 }
